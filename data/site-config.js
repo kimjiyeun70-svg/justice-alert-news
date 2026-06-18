7ae7292c52c0e9a54d0c07bbb97503f42e3549cf
kimjiyeun70-svg <kimjiyeun70@gmail.com>
2026-06-18 09:19:38 +0900

    AutoPilot: update data/site-config.js

---
+++ data/site-config.js
@@ -0,0 +1,7 @@
+window.siteConfig = {
+  "siteName": "Justice Alert News",
+  "primaryColor": "#b91c1c",
+  "bgMain": "#fdf2f2",
+  "fontHeading": "'Playfair Display', serif",
+  "layoutStyle": "serif"
+};
